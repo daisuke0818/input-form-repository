003267186322b8795822328a59cfb711b806db41
daisuke0818 <faculty.miyakoda@gmail.com>
2020-12-08 23:09:36 +0900

    commit!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterInputComponent } from './internal/account-manage/container/register-input/register-input.component';
+import { InputCardComponent } from './internal/account-manage/components/input-card/input-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegisterInputComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RegisterInputComponent, InputCardComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
