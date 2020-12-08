6bbed1b1d3535646385e3197335bb5541da23d7e
daisuke0818 <faculty.miyakoda@gmail.com>
2020-12-08 23:33:42 +0900

    updateしたいのでcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+
 import { AppComponent } from './app.component';
 import { RegisterInputComponent } from './internal/account-manage/container/register-input/register-input.component';
 import { InputCardComponent } from './internal/account-manage/components/input-card/input-card.component';
 
 @NgModule({
   declarations: [AppComponent, RegisterInputComponent, InputCardComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MatFormFieldModule, MatLabel],
   providers: [],
   bootstrap: [AppComponent],
 })
